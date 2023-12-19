982380e7cc5c6dd56a9d75f23a4fd73026fb19ef
Anton Kuts <anton.a.kuts@gmail.com>
2023-12-19 22:09:45 +0200

    completed the 'Bookmark' layout

--- Bookmark/js/script.js
+++ Bookmark/js/script.js
@@ -1,5 +1,8 @@
 const tabs = document.querySelectorAll(".tab");
 const panels = document.querySelectorAll(".panel");
+const btn = document.getElementById("menu-btn");
+const menu = document.getElementById("menu");
+const logo = document.getElementById("logo");
 
 // Tabs menu event listener
 tabs.forEach((tab) => tab.addEventListener("click", onTanClick));
@@ -26,4 +29,19 @@ function onTanClick(event) {
     .getElementById("panels")
     .getElementsByClassName(classString)[0]
     .classList.remove("hidden");
+}
+
+// Hamburger btn listener
+btn.addEventListener("click", navToggle);
+
+function navToggle() {
+  btn.classList.toggle("open");
+  menu.classList.toggle("flex");
+  menu.classList.toggle("hidden");
+
+  if (menu.classList.contains("flex")) {
+    logo.setAttribute("src", "./images/logo-bookmark-footer.svg");
+  } else {
+    logo.setAttribute("src", "./images/logo-bookmark.svg");
+  }
 }
