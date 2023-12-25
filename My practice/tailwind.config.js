0382fcadc149a3f69452d9b8972cdc1f069605e3
Anton Kuts <anton.a.kuts@gmail.com>
2023-12-25 22:16:08 +0200

    init my own practice

---
+++ My practice/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Montserrat", "sans-serif"],
+        opensans: ["Yeseva One", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
