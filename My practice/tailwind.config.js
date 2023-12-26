252c36a58c8ebf26b9cba5724928d7dac40b13e4
Anton Kuts <anton.a.kuts@gmail.com>
2023-12-26 22:28:42 +0200

    completed header secrion

--- My practice/tailwind.config.js
+++ My practice/tailwind.config.js
@@ -2,10 +2,23 @@
 module.exports = {
   content: ["./*html"],
   theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+			xl: "1200px",
+      xl2: "1440px"
+    },
     extend: {
+      colors: {
+        myBrown: "#A55335",
+        myBlue: "005A77",
+        darkGrayishBlue: "hsl(210, 10%, 33%)",
+        grayishBlue: "hsl(201, 11%, 66%)",
+      },
       fontFamily: {
-        sans: ["Montserrat", "sans-serif"],
-        opensans: ["Yeseva One", "sans-serif"],
+        main: ["Montserrat", "sans-serif"],
+        yeseva: ["Yeseva One", "sans-serif"],
       },
     },
   },
